755cb7264a8f224f4f740f2dea9268046b05fdf8
netomilfont <netomilfont@hotmail.com>
2023-03-09 12:52:14 -0300

    feat: add a model of addresses

--- addresses/models.py
+++ addresses/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+class Adress(models.Model):
+    id = models.UUIDField(
+        primary_key=True, 
+        default=uuid.uuid4, 
+        editable=False)
+    street = models.CharField(max_length=200)
+    number = models.IntegerField()
+    complement = models.CharField(max_length=200)
+    postal_code = models.CharField(max_length=20)
+    neighborhood = models.CharField(max_length=50)
+    city = models.CharField(max_length=30)
+    state = models.CharField(max_length=20)
