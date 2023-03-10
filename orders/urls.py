381116a6468ba29671e2efe47d64bf8acaa1b461
netomilfont <netomilfont@hotmail.com>
2023-03-10 16:56:27 -0300

    feat: add urls patterns of orders

---
+++ orders/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+from rest_framework_simplejwt import views as jwt_views
+
+urlpatterns = [
+    path("orders/", views..as_view()),
+    path("orders/<int:pk>/", views..as_view()),
+]
