550ddb068f2396fc35e1f27107c26ca503df6c2a
netomilfont <netomilfont@hotmail.com>
2023-03-10 16:23:22 -0300

    feat: add the models of Orders and your relationship with products

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+class StatusOptions(models.TextChoices):
+    DEFAULT = "Pedido realizado"
+    EM_ANDAMENTO = "Em andamento"
+    ENTREGUE = "Entregue"
+
+
+class Order(models.Model):
+    id = models.UUIDField(
+        primary_key=True, 
+        default=uuid.uuid4, 
+        editable=False) 
+    order_at = models.DateTimeField()
+    total_price = models.DecimalField(max_digits=5, decimal_places=2)
+    status = models.CharField(max_length=30, default=StatusOptions.DEFAULT, choices=StatusOptions.choices)
+
+    products = models.ManyToManyField(
+        "products.Product",
+        through="orders.OrderProducts",
+        related_name="product_orders"
+    )
+
+
+class OrderProducts(models.Model):
+    id = models.UUIDField(
+        primary_key=True, 
+        default=uuid.uuid4, 
+        editable=False)
+    quantity = models.IntegerField()
+
+    order = models.ForeignKey(
+        "orders.Order",
+        on_delete=models.CASCADE,
+        related_name="order_products"
+    )
+
+    product = models.ForeignKey(
+        "products.Product",
+        on_delete=models.CASCADE,
+        related_name="user_order_products",
+    )
+
+    def __str__(self) -> str:
+        return f"<ProductOrder [{self.id}] - {self.quantity}>"
+
+
