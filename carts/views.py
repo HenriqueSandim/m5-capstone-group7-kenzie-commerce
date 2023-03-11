fe56e140ef25cd676ae21349c3f11589036bacd3
Saul Gomes <klowd.bk@gmail.com>
2023-03-11 16:40:45 -0300

    models/views/serializer completos

--- carts/views.py
+++ carts/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from .models import Cart
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from .serializer import CartSerializer
+from rest_framework.generics import CreateAPIView, RetrieveUpdateDestroyAPIView
 
-# Create your views here.
+
+class CartView(CreateAPIView):
+    serializer_class = CartSerializer
+
+
+class CartDetailView(RetrieveUpdateDestroyAPIView):
+    authentication_classes = [JWTAuthentication]
+
+    queryset = Cart.objects.all()
+
+    serializer_class = CartSerializer
