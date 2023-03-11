e96287543bee3fa48c8d2a6fae85115c84becb7f
Dexter <neimestre@yahoo.com.br>
2023-03-11 11:25:50 -0300

    feat: products app

--- products/views.py
+++ products/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Product
+from .serializer import ProductSerializer
 
-# Create your views here.
+
+class ProductView(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class ProductDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_url_kwarg = "pk"
