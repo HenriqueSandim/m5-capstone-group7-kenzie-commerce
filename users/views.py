74114ab259accac571cbd93fb85c1de130ef3e0c
Henrique Sandim <henrique.p.a.sandim@gmail.com>
2023-03-12 10:34:57 -0300

    feat: user views done

--- users/views.py
+++ users/views.py
@@ -1,15 +1,23 @@
-from django.shortcuts import render
-
 from rest_framework import generics
 
 from .models import User
 from .serializer import UserSerializer
+from .permissions import IsAdminOrOwner
+
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
 
 # Create your views here.
-class UsersViews(generics.ListCreateAPIView):
+class UsersViews(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
-    authentication_classes = []
-    permission_classes = []
+
+class UserManageViews(generics.RetrieveUpdateDestroyAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAdminOrOwner]
+
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    lookup_url_kwarg = "user_id"
