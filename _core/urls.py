634e7bdb9f7e825814d2121d1df84591de8fc7fc
Henrique Sandim <henrique.p.a.sandim@gmail.com>
2023-03-13 13:01:21 -0300

    feat: addresses added to urls

--- _core/urls.py
+++ _core/urls.py
@@ -23,6 +23,7 @@ from drf_spectacular.views import (
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("users.urls")),
+    path("api/", include("addresses.urls")),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
