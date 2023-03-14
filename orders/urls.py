27b3843352c86071ccfd96ac44c967522cd19685
netomilfont <netomilfont@hotmail.com>
2023-03-14 18:48:03 -0300

    fix: fix orders urls

--- orders/urls.py
+++ orders/urls.py
@@ -3,5 +3,6 @@ from . import views
 from rest_framework_simplejwt import views as jwt_views
 
 urlpatterns = [
-    path("orders/", views.OrderProductsView.as_view()),
+    path("orders/", views.OrderView.as_view()),
+    path("orders/<int:pk>/", views.OrderDetailView.as_view()),
 ]
